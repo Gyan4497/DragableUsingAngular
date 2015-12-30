7f1849f6870b973cae3a6bd56d070885f6760456
Gyan4497 <gyanprakash92@gmail.com>
2015-12-30 10:22:08 +0530

    first draggable

---
+++ DraggableGadgetsUsingAngular/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DraggableGadgetsUsingAngular")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DraggableGadgetsUsingAngular")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6079dc3f-4506-45c2-8b2d-9ec05fa1bf5d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DraggableGadgetsUsingAngular/angularscript.js
@@ -0,0 +1,27 @@
+﻿var myApp = angular.module('myApp', []);
+
+function MyCtrl($scope) {
+    $scope.divwidth = 400;
+    $scope.divheight = 300;
+    $scope.show = true;
+    $scope.divtop = 80;
+    $scope.divleft = 50;
+    $scope.divtop2 = 180;
+    $scope.divleft2 = 150;
+};
+
+myApp.directive('draggable', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            element.draggable({
+                cursor: "move",
+                stop: function (event, ui) {
+                    scope[attrs.xpos] = ui.position.left;
+                    scope[attrs.ypos] = ui.position.top;
+                    scope.$apply();
+                }
+            });
+        }
+    };
+});
